d62ab6dfc69ba5b19547f07facee615256b49fde
careydevelopment <mrbrianmcarey@gmail.com>
2021-03-23 19:32:12 -0400

    initial oauth work

--- src/main/java/com/careydevelopment/ecosystem/email/controller/GmailController.java
+++ src/main/java/com/careydevelopment/ecosystem/email/controller/GmailController.java
@@ -2,30 +2,43 @@ package com.careydevelopment.ecosystem.email.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.careydevelopment.ecosystem.email.model.User;
+import com.careydevelopment.ecosystem.email.util.AuthorizationUtil;
+import com.careydevelopment.ecosystem.email.util.GmailUtil;
+import com.careydevelopment.ecosystem.email.util.GoogleOauthUtil;
 
 @Controller
 public class GmailController {
 
     private static final Logger LOG = LoggerFactory.getLogger(GmailController.class);
 
+    @Autowired
+    private GmailUtil gmailUtil;
     
-    @GetMapping("/{id}/email/getAuthorizationCode")
-    public ResponseEntity<?> getAuthorizationCode(@PathVariable("id") String id, @RequestParam("baseRedirectUrl") String baseRedirectUrl) {
+    @Autowired
+    private GoogleOauthUtil googleOauthUtil;
+    
+    @Autowired
+    private AuthorizationUtil authUtil;
+
+    
+    
+    @GetMapping("/email/authorizationCode")
+    public ResponseEntity<?> getAuthorizationCode(@RequestParam("baseRedirectUrl") String baseRedirectUrl) {
         LOG.debug("Got in here " + baseRedirectUrl);    
         
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = (User)authentication.getPrincipal();
+        User currentUser = authUtil.getCurrentUser();
         
         String url = googleOauthUtil.getAuthorizationCode(currentUser.getId(), baseRedirectUrl);            
-        return ResponseEntityUtil.createResponseEntity(url , HttpStatus.OK);
+        return ResponseEntity.ok(url);
     }
 }
