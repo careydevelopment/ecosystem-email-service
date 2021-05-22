87b0c01292a339e89c9678c8d1709320e38e6335
careydevelopment <mrbrianmcarey@gmail.com>
2021-05-22 08:21:07 -0400

    security refactoring

--- src/main/java/com/careydevelopment/ecosystem/email/config/WebSecurityConfig.java
+++ src/main/java/com/careydevelopment/ecosystem/email/config/WebSecurityConfig.java
@@ -1,56 +1,16 @@
 package com.careydevelopment.ecosystem.email.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
+import us.careydevelopment.ecosystem.jwt.config.JwtOnlySecurityConfig;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends JwtOnlySecurityConfig {
 
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    
-    @Autowired
-    private JwtAuthenticationProvider jwtAuthenticationProvider;
-    
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        
-        return new CorsFilter(source);
-    }
-    
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-            .cors().and()
-            .csrf().disable()
-            .addFilter(new BearerTokenAuthenticationFilter(authenticationManager()))
-            .authorizeRequests() 
-            .anyRequest().access("hasAuthority('CAREYDEVELOPMENT_CRM_USER')").and()
-            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(jwtAuthenticationProvider);
+    public WebSecurityConfig(@Autowired JwtAuthenticationProvider jwtAuthenticationProvider) {
+        this.authenticationProvider = jwtAuthenticationProvider;
     }
 }
